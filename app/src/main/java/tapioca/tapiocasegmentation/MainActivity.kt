8e8b2dada5782e03760f67f81dd106a31059e3df
Akira Hashimto <akirasyoku1234@gmail.com>
2019-09-07 16:25:30 +0900

    splash、カメラからの画像受け取りのUI調整

--- app/src/main/java/tapioca/tapiocasegmentation/MainActivity.kt
+++ app/src/main/java/tapioca/tapiocasegmentation/MainActivity.kt
@@ -4,25 +4,20 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Matrix
-import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.os.Handler
 import android.provider.MediaStore
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import org.opencv.android.OpenCVLoader
 import org.opencv.android.Utils
-import org.opencv.imgproc.GeneralizedHough
+import org.opencv.core.Mat
+import org.opencv.core.Point
+import org.opencv.core.Scalar
 import org.opencv.imgproc.Imgproc
-import android.R.attr.y
-import android.R.attr.x
-import org.opencv.core.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -34,9 +29,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        println(OpenCVLoader.OPENCV_VERSION)
 
-        takeButton.setOnClickListener {
+        OpenCVLoader.initDebug()
+        Handler().postDelayed({
             if (ContextCompat.checkSelfPermission(
                     this,
                     android.Manifest.permission.CAMERA
@@ -44,63 +39,24 @@ class MainActivity : AppCompatActivity() {
             ) {
                 startActivityForResult(takeIntent, RESULT_CAMERA)
             } else {
-                if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.CAMERA)) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        this,
+                        android.Manifest.permission.CAMERA
+                    )
+                ) {
                     AlertDialog.Builder(this)
                         .setMessage("許可してくれないんで画面を閉じます。")
                         .setPositiveButton("閉じる") { _, _ -> finish() }
                         .show()
                 } else {
-                    ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), 0)
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(android.Manifest.permission.CAMERA),
+                        0
+                    )
                 }
             }
-        }
-
-        httpButton.setOnClickListener {
-            GlobalScope.launch(Dispatchers.IO) {
-                val bitmap = (imageView.drawable as BitmapDrawable).bitmap
-//                var tapimap: Bitmap? = null
-//                withContext(Dispatchers.IO) {
-//                    tapimap = HttpUtil.getTapiocaImage(
-//                        "https://us-central1-tapiocachallenge.cloudfunctions.net/getTapioca",
-//                        bitmap
-//                    )
-//                }
-//                val createScaledBitmap = Bitmap.createScaledBitmap(bitmap, 270, 480, true)
-//                480 * 270
-                println("initDebug ${OpenCVLoader.initDebug()}")
-                val inputFrame = Mat()
-                Utils.bitmapToMat(bitmap, inputFrame)
-                inputFrame.reshape(2)
-//                val frame480 = Mat.zeros(480, 640, CvType.CV_8UC3)
-//                Imgproc.resize(inputFrame, frame480, Size(), 1.0, 1.0, Imgproc.INTER_NEAREST)
-                Imgproc.cvtColor(inputFrame, inputFrame, Imgproc.COLOR_RGB2GRAY)
-//                Imgproc.threshold(
-//                    frame480, frame480, 0.0, 255.0,
-//                    Imgproc.THRESH_BINARY or Imgproc.THRESH_OTSU
-//                )
-                val circles = Mat()
-                Imgproc.HoughCircles(inputFrame, circles, Imgproc.CV_HOUGH_GRADIENT,
-                    1.0, 100.0,24.0,40.0,40,110)
-                println(circles.cols())
-                val pt = Point()
-// 検出した直線上を緑線で塗る
-                Imgproc.cvtColor(inputFrame, inputFrame, Imgproc.COLOR_GRAY2BGR)
-                for (i in 0 until circles.cols()) {
-                    val data = circles.get(0, i)
-                    pt.x = data[0]
-                    pt.y = data[1]
-                    val rho = data[2]
-                    Imgproc.circle(inputFrame, pt, rho.toInt(), Scalar(0.0, 200.0, 0.0), 5)
-                }
-                Utils.matToBitmap(inputFrame, bitmap)
-                imageView.setImageBitmap(bitmap)
-//                GlobalScope.launch(Dispatchers.Main) {
-//                    imageView.setImageBitmap(createScaledBitmap)
-//                }
-
-//                val tapimap = HttpUtil.getTapiocaImage("https://us-central1-logical-waters-250102.cloudfunctions.net/tapicathon_python37", bitmap)
-            }
-        }
+        }, 2000)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -109,25 +65,46 @@ class MainActivity : AppCompatActivity() {
                 if (it is Bitmap) {
                     val matrix = Matrix()
                     matrix.postRotate(90F)
-                    val rotatedBitmap = Bitmap.createBitmap(it, 0, 0, it.width, it.height, matrix, true)
-                    GlobalScope.launch(Dispatchers.IO) {
-                        var tapimap: Bitmap? = null
-                        withContext(Dispatchers.IO) {
-                            tapimap = HttpUtil.getTapiocaImage(
-                                "https://us-central1-tapiocachallenge.cloudfunctions.net/getTapioca",
-                                rotatedBitmap
-                            )
-                        }
-                        GlobalScope.launch(Dispatchers.Main) {
-                            imageView.setImageBitmap(tapimap)
-                        }
+                    val rotatedBitmap =
+                        Bitmap.createBitmap(it, 0, 0, it.width, it.height, matrix, true)
+                    val inputFrame = Mat()
+                    Utils.bitmapToMat(rotatedBitmap, inputFrame)
+                    inputFrame.reshape(2)
+                    Imgproc.cvtColor(inputFrame, inputFrame, Imgproc.COLOR_RGB2GRAY)
+                    val circles = Mat()
+                    Imgproc.HoughCircles(
+                        inputFrame, circles, Imgproc.CV_HOUGH_GRADIENT,
+                        1.0, 100.0, 24.0, 40.0, 40, 110
+                    )
+                    println(circles.cols())
+                    val pt = Point()
+// 検出した直線上を緑線で塗る
+                    Imgproc.cvtColor(inputFrame, inputFrame, Imgproc.COLOR_GRAY2BGR)
+                    for (i in 0 until circles.cols()) {
+                        val data = circles.get(0, i)
+                        pt.x = data[0]
+                        pt.y = data[1]
+                        val rho = data[2]
+                        Imgproc.circle(
+                            inputFrame,
+                            pt,
+                            rho.toInt(),
+                            Scalar(0.0, 200.0, 0.0),
+                            5
+                        )
                     }
+//                    Utils.bitmapToMat(rotatedBitmap, inputFrame)
+//                    splash.setImageBitmap(rotatedBitmap)
                 }
             }
         }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         when (requestCode) {
             0 -> {
                 if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
